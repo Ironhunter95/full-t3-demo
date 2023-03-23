82fe01d1b842d4bde236056a17a90917e773538b
Mohammed Aboukhatwa <ironhunter90@gmail.com>
2023-03-24 01:13:03 +0200

    Sign in and Sign out with clerk

---
+++ src/middleware.ts
@@ -0,0 +1,13 @@
+//this file means that every request will process this middleware
+
+import { withClerkMiddleware } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
+ 
+export default withClerkMiddleware(() => {
+  return NextResponse.next();
+});
+ 
+// Stop Middleware running on static files
+export const config = {
+  matcher: "/((?!_next/image|_next/static|favicon.ico).*)",
+};
